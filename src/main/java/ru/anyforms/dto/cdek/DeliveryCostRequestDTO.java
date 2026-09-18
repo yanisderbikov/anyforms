b7eccfe7d9bd9d9266065dc3e50111f79292baaa
Yan Derbikov <yanisderbikov@gmail.com>
2026-09-18 15:30:58 +0500

    items not null

--- src/main/java/ru/anyforms/dto/cdek/DeliveryCostRequestDTO.java
+++ src/main/java/ru/anyforms/dto/cdek/DeliveryCostRequestDTO.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import ru.anyforms.dto.payment.CartItemDTO;
 
@@ -16,7 +17,7 @@ public class DeliveryCostRequestDTO {
     @NotEmpty
     @Valid
     @Schema(description = "Позиции корзины")
-    private List<CartItemDTO> items;
+    private List<@NotNull CartItemDTO> items;
 
     @NotBlank
     @Schema(description = "Код ПВЗ СДЭК получателя (из /api/cdek/pvz)")
